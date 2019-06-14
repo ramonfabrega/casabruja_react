963c991441a112d63fe3dd2db946517805c6b3df
Ramon Fabrega <ramonfabrega0@gmail.com>
2019-06-14 15:47:25 -0500

    add images and favicon

--- client/src/App.js
+++ client/src/App.js
@@ -16,45 +16,39 @@ import Profile from './components/profile/Profile';
 
 import ResponsiveLayout from './components/common/ResponsiveLayout';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAuthenticated: localStorage.getItem('uid') || ''
-    };
-  }
-  // console.log('store start');
-  // console.log(store);
-  // console.log('end');
-  render() {
-    return (
-      <div className='App'>
-        <Provider store={store}>
-          <Router>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column mobile={14} tablet={13} computer={14}>
-                  <Route exact path='/' component={ResponsiveLayout} />
-                  <Route path='/login' component={Login} />
-                  <Route path='/register' component={Register} />
-                  <Route path='/logout' component={Logout} />
-                  <PrivateRoute
-                    path='/profile'
-                    component={Profile}
-                    authUID={this.state.isAuthenticated}
-                  />
-                </Grid.Column>
-                <Grid.Column mobile={2} tablet={3} computer={2}>
-                  <Navbar />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Router>
-        </Provider>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <Provider store={store}>
+        <Router>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column
+                mobile={14}
+                tablet={13}
+                computer={14}
+                style={{ padding: 15, margin: 0, paddingLeft: 30 }}
+              >
+                <Route exact path='/' component={ResponsiveLayout} />
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route path='/logout' component={Logout} />
+                <PrivateRoute path='/profile' component={Profile} />
+              </Grid.Column>
+              <Grid.Column
+                mobile={2}
+                tablet={3}
+                computer={2}
+                style={{ padding: 0, margin: 0 }}
+              >
+                <Navbar />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Router>
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
