3014f8741c0a00fca334ea4e9e3832c30896ad94
Ramon Fabrega <ramonfabrega0@gmail.com>
2019-06-18 08:26:24 -0500

    dashboard structuring

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,95 +1,37 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-
-import fula_1 from '../../img/products/fula_1.jpg';
-import chivo_1 from '../../img/products/chivo_1.jpg';
-import sirfrancis_1 from '../../img/products/sirfrancis_1.jpg';
-import tulivieja_1 from '../../img/products/tulivieja_1.jpg';
+import { Grid, Image } from 'semantic-ui-react';
 
 import Product from '../product/Product';
+import Checkout from '../common/Checkout';
+import { products } from '../mock/data';
 
-// 1024px (laptop) & above: col width = 4
-// below: col width = 8
-// mobile => < 425px => col_width: 12
-// tablet => < 768px => col_width: 8
-// laptop => < 1024px => col_wdith: 4
-
-const products = [
-  {
-    name: 'Fula',
-    style: 'Blonde Ale',
-    price: 1.5,
-    image: fula_1,
-    abv: 2.4,
-    ibu: 24
-  },
-  {
-    name: 'Chivoperro',
-    style: 'India Pale Ale',
-    price: 2,
-    image: chivo_1,
-    abv: 6.1,
-    ibu: 49
-  },
-  {
-    name: 'Sir Francis',
-    style: 'Red Ale',
-    price: 2.5,
-    image: sirfrancis_1,
-    abv: 5.5,
-    ibu: 26
-  },
-  {
-    name: 'Tulivieja',
-    style: 'Imperial India Pale Ale',
-    price: 3.0,
-    image: tulivieja_1,
-    abv: 8.4,
-    ibu: 101
-  },
-  {
-    name: 'Tulivieja',
-    style: 'Imperial India Pale Ale',
-    price: 3.0,
-    image: tulivieja_1,
-    abv: 8.4,
-    ibu: 101
-  },
-  {
-    name: 'Tulivieja',
-    style: 'Imperial India Pale Ale',
-    price: 3.0,
-    image: tulivieja_1,
-    abv: 8.4,
-    ibu: 101
-  },
-  {
-    name: 'Tulivieja',
-    style: 'Imperial India Pale Ale',
-    price: 3.0,
-    image: tulivieja_1,
-    abv: 8.4,
-    ibu: 101
-  },
-  {
-    name: 'Tulivieja',
-    style: 'Imperial India Pale Ale',
-    price: 3.0,
-    image: tulivieja_1,
-    abv: 8.4,
-    ibu: 101
-  }
-];
+import header1 from '../../img/header1.png';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <Grid centered>
-        {products.map((p, i) => (
-          <Grid.Column mobile={12} tablet={8} computer={4} key={i}>
-            <Product product={p} />
+        <Grid.Row>
+          <Grid.Column computer={10} tablet={15} mobile={15}>
+            <Image src={header1} />
           </Grid.Column>
-        ))}
+          <Grid.Column computer={6} tablet={15} mobile={15} textAlign='right'>
+            <Checkout />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          {products.map((p, i) => (
+            <Grid.Column
+              mobile={12}
+              tablet={8}
+              computer={4}
+              key={i}
+              style={{ marginBottom: 25 }}
+            >
+              <Product product={p} />
+            </Grid.Column>
+          ))}
+        </Grid.Row>
       </Grid>
     );
   }
---
+++ src/components/mock/data.js
@@ -0,0 +1,76 @@
+import fula_1 from '../../img/products/fula_1.jpg';
+import chivo_1 from '../../img/products/chivo_1.jpg';
+import sirfrancis_1 from '../../img/products/sirfrancis_1.jpg';
+import tulivieja_1 from '../../img/products/tulivieja_1.jpg';
+import ttt_1 from '../../img/products/ttt_1.jpg';
+import chitra_1 from '../../img/products/chitra_1.jpg';
+import sandokan_1 from '../../img/products/sandokan_1.jpg';
+import gose_limon_1 from '../../img/products/gose_limon_1.jpg';
+
+export const products = [
+  {
+    name: 'Fula',
+    style: 'Blonde Ale',
+    price: 1.5,
+    image: fula_1,
+    abv: 2.4,
+    ibu: 24
+  },
+  {
+    name: 'Tres Tristes Tigres',
+    style: 'Witbier',
+    price: 2.0,
+    image: ttt_1,
+    abv: 4.6,
+    ibu: ''
+  },
+  {
+    name: 'Chivoperro',
+    style: 'India Pale Ale',
+    price: 2,
+    image: chivo_1,
+    abv: 6.1,
+    ibu: 49
+  },
+  {
+    name: 'Chitra',
+    style: 'Pale Ale',
+    price: 3.0,
+    image: chitra_1,
+    abv: '',
+    ibu: ''
+  },
+  {
+    name: 'Sir Francis',
+    style: 'Red Ale',
+    price: 2.5,
+    image: sirfrancis_1,
+    abv: 5.5,
+    ibu: 26
+  },
+
+  {
+    name: 'Tulivieja',
+    style: 'Imperial India Pale Ale',
+    price: 3.0,
+    image: tulivieja_1,
+    abv: 8.4,
+    ibu: 101
+  },
+  {
+    name: 'Gose',
+    style: 'Limon Kaffir',
+    price: 3.0,
+    image: gose_limon_1,
+    abv: '',
+    ibu: ''
+  },
+  {
+    name: 'Sandokan',
+    style: 'New England IPA',
+    price: 3.0,
+    image: sandokan_1,
+    abv: 8.4,
+    ibu: 101
+  }
+];
