af169fb0b7a096514e7e53d9f36a3cc42ba7bd3b
Ramon Fabrega <ramonfabrega0@gmail.com>
2019-06-22 15:08:26 -0500

    cleanup firebase

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 import 'semantic-ui-less/semantic.less';
 
-ReactDOM.render(
-  // <FirebaseContext.Provider value={new Firebase()}>
-  <App />,
-  // </FirebaseContext.Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
